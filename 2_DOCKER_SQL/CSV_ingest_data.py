4329b394ae36b86225f6b003a355989778eb87d5
Iskanderrus <a.n.chasovskoy@gmail.com>
2023-01-19 16:55:25 +0300

    Working csv ingest directrly with python.

--- 2_DOCKER_SQL/CSV_ingest_data.py
+++ 2_DOCKER_SQL/CSV_ingest_data.py
@@ -1,25 +1,50 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+import argparse
 import os
 import pandas as pd
 from sqlalchemy import create_engine
 
-URL = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
 
-csv_name = 'csv_output_data.csv'
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
 
-os.system(f"wget {URL} -O {csv_name}")
-df = pd.read_csv(csv_name, low_memory=False, compression='gzip')
+    #    URL = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
 
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+    csv_name = 'csv_output_data.csv'
 
-print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
+    os.system(f"wget {url} -O {csv_name}")
+    df = pd.read_csv(csv_name, low_memory=False, compression='gzip')
 
-df.head(0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    print(pd.io.sql.get_schema(df, name=table_name, con=engine))
 
-df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+    df.head(0).to_sql(name=table_name, con=engine, if_exists='replace')
 
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ingest CSV data to PostgreSQL')
+
+    parser.add_argument('--user', help='user name for postgres')
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table_name', help='name of the table where we will write the results to')
+    parser.add_argument('--url', help='url of the parquet file')
+
+    args = parser.parse_args()
+
+    main(args)
