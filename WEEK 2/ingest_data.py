5fc3d88484664fb300e86a9c041b62971b238f3d
Iskanderrus <a.n.chasovskoy@gmail.com>
2023-01-30 19:20:42 +0300

    ingest_data.py - final corrections to be run on the previous week yaml settings

--- WEEK 2/ingest_data.py
+++ WEEK 2/ingest_data.py
@@ -1,12 +1,12 @@
 import os
-from time import time
-
+import argparse
+from prefect import flow, task
 import pandas as pd
 from sqlalchemy import create_engine
 
 
+@task(log_prints=True, retries=3)
 def ingest_data(user, password, host, port, db, table_name, url):
-
     if url.endswith('.csv.gz'):
         csv_name = 'yellow_tripdata_2021-01.csv.gz'
     else:
@@ -15,7 +15,7 @@ def ingest_data(user, password, host, port, db, table_name, url):
     os.system(f"wget {url} -O {csv_name}")
     postgres_url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
 
-    engine = create_engine(postgres_url)
+    engine = create_engine(postgres_url, pool_pre_ping=True)
 
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
@@ -27,34 +27,19 @@ def ingest_data(user, password, host, port, db, table_name, url):
     df.head(0).to_sql(name=table_name, con=engine, if_exists='replace')
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
-    while True:
 
-        try:
-            t_start = time()
-
-            df = next(df_iter)
-
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
-            df.to_sql(name=table_name, con=engine, if_exists='append')
-
-            t_end = time()
-
-            print(f'inserted another chunk, took {round(t_end - t_start, 3)} second(s)')
-
-        except StopIteration:
-            print('Finished ingesting data into the postgres database')
-            break
-
-
-if __name__ == '__main__':
-    user = 'postgres'
-    password = 'admin'
+@flow(name="Ingest Flow")
+def main():
+    user = 'root'
+    password = 'root'
     host = 'localhost'
-    port = '5433'
-    db = 'my_taxi'
+    port = '5432'
+    db = 'ny_taxi'
     table_name = 'yellow_taxi_trips'
     csv_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz'
 
     ingest_data(user=user, password=password, host=host, port=port, db=db, table_name=table_name, url=csv_url)
+
+
+if __name__ == '__main__':
+    main()
