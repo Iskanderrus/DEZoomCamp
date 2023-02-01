691700c3799e5363169258f9039859f98569ad99
Alexander Chasovskoy <a.n.chasovskoy@gmail.com>
2023-02-01 15:31:33 +0000

    corrected path to match local environement

--- WEEK_2/02_gcp/etl_gcs_to_bq.py
+++ WEEK_2/02_gcp/etl_gcs_to_bq.py
@@ -9,8 +9,8 @@ def extract_from_gcs(color: str, year: int, month: int) -> Path:
     '''Download trip data from GCS'''
     gcs_path = f'data/{color}/{color}_tripdata_{year}-{month:02}.parquet'
     gcs_block = GcsBucket.load('dez-gcs')
-    gcs_block.get_directory(from_path=gcs_path, local_path=f'/home/alex/DEZoomCamp/WEEK_2/02_gcp/data/')
-    return Path(f'/home/alex/DEZoomCamp/WEEK_2/02_gcp/data/{gcs_path}')
+    gcs_block.get_directory(from_path=gcs_path, local_path=f'/home/alex/DEZoomCamp/WEEK_2/02_gcp/')
+    return Path(f'/home/alex/DEZoomCamp/WEEK_2/02_gcp/{gcs_path}')
 
 
 
