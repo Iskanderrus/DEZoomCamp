f6fc7be30246f9a32cc281d946ce5bcf22dba1b6
Alexander Chasovskoy <a.n.chasovskoy@gmail.com>
2023-02-02 20:12:50 +0000

    final version of parametrized flow

--- WEEK_2/02_gcp/parameterized_flow.py
+++ WEEK_2/02_gcp/parameterized_flow.py
@@ -2,16 +2,13 @@ from pathlib import Path
 import pandas as pd
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
-from prefect.tasks import task_input_hash
+from random import randint 
+from prefect.tasks import task_import_hash
 from datetime import timedelta
-import os
 
 
-@task(
-    retries=3,
-    cache_key_fn=task_input_hash,
-    cache_expiration=timedelta(days=1))
-def fetch(dataset_url):
+@task(retries=3, cache_key_fn=task_import_hash, cache_expiration=timedelta(days=1))
+def fetch(dataset_url): 
     """
     Read taxi data from web to Pandas DataFrame
     """
@@ -21,47 +18,41 @@ def fetch(dataset_url):
 
     df = pd.read_csv(dataset_url, low_memory=True)
 
-    return df
+    return df 
 
 
 @task(log_prints=True)
-def clean(df=pd.DataFrame) -> pd.DataFrame:
+def clean(df = pd.DataFrame) -> pd.DataFrame:
     """
     Fix some dtype issues
-    """
+    """ 
     df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
     df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
     print(df.head(2))
     print(f'columns: {df.dtypes}')
     print(f'shape of the dataframe:\n\t\t\t\t\t\t\t\t\t\trows:{df.shape[0]}\n\t\t\t\t\t\t\t\t\t\tcolumns:{df.shape[1]}')
-    return df
+    return df 
 
 
 @task()
-def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path: 
     """Write DataFrame out as parquet file"""
-    parent_dir = f'WEEK_2/02_gcp/data/{color}'
-    if os.path.exists(parent_dir): 
-        path = os.path.join(parent_dir, f'{dataset_file}.parquet')
-    else: 
-        path = Path(os.path.join(parent_dir, f'{dataset_file}.parquet')).mkdir(parents=True, exist_ok=True)
+    path = Path(f'data/{color}/{dataset_file}.parquet')
     df.to_parquet(path, compression='gzip')
     return path
 
-
 @task
-def write_gcs(path: Path) -> None:
+def write_gcs(path: Path) -> None: 
     """Uploading local parquet file to Google Cloud Storage"""
     gcs_block = GcsBucket.load('dez-gcs')
     gcs_block.upload_from_path(
-        from_path=f'{path}',
+        from_path=f'{path}', 
         to_path=path
     )
     return
 
-
 @flow()
-def etl_web_to_gcs(color: str, year: int, month: int):
+def etl_web_to_gcs(year: int, month: int, color: str): 
     """
     The main ETL function
     """
@@ -72,21 +63,20 @@ def etl_web_to_gcs(color: str, year: int, month: int):
     df = fetch(dataset_url)
     cleaned_df = clean(df)
     path = write_local(cleaned_df, color, dataset_file)
-    #write_gcs(path)
+    write_gcs(path)
 
 
 @flow()
 def etl_parent_flow(
-        months: list[int] = [1, 2],
-        year: int = 2021,
-        color: str = 'yellow'
-):
-    for month in months:
-        etl_web_to_gcs(color=color, year=year, month=month)
+    months: list = [1, 2], 
+    year: int = 2021,
+    color: str = 'yellow'
+): 
+    for month in months: 
+        etl_web_to_gcs(month=month, year=year, color=color)
 
-
-if __name__ == '__main__':
+if __name__ == '__main__': 
     color = 'yellow'
-    months = [1, 2, 3]
+    months = [1, 2, 3, 4]
     year = 2021
-    etl_parent_flow(color=color, year=year, months=months)
+    etl_parent_flow(months=months, color=color, year=year)
