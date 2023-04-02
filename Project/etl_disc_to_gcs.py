4002f21c9462734936687baa99153c2da4687995
Aleex <a.n.chasovskoy@gmail.com>
2023-04-02 16:43:31 +0000

    final version of the first etl

--- Project/etl_disc_to_gcs.py
+++ Project/etl_disc_to_gcs.py
@@ -1,10 +1,42 @@
 from pathlib import Path
 import pandas as pd
-from prefect import flow, tasc
+from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
-from .imdb.imdb.spiders.imdb_spider import ImdbSpiderSpider
+# from imdb.imdb.spiders.imdb_spider import ImdbSpiderSpider
+# from scrapy.crawler import CrawlerProcess
+from datetime import datetime
+import os
 
-def etl_dick_to_gcs(): 
+
+# @task
+# def crawl_data(): 
+#     date = datetime.now().strftime('%Y%m%d')
+#     process = CrawlerProcess(settings={
+#     'FEED_URI': f'./data/{date}_films.csv', 
+#     'FEED_FORMAT': 'csv'
+#     })
+
+#     process.crawl(ImdbSpiderSpider)
+#     process.start()
+#     full_path = os.path.abspath(f'./data/{date}_films.csv')
+#     return full_path
+
+
+@task(retries=3)
+def fetch(file_name): 
+    """Read data into pandas dataframe"""
+    df = pd.read_csv(file_name, low_memory=True)
+    print('Data loaded into pandas')
+    return df
+
+@flow
+def etl_disc_to_gcs(): 
     """"Main ETL function"""
-    
-    data_set = "./imdb/imdb/data/films.scv"
+
+    data_set = "./imdb/imdb/data/films.csv"
+    df = fetch(data_set)
+
+
+
+if __name__ == '__main__': 
+    etl_disc_to_gcs()
