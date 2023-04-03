0c2ca00588fffc705b2ee447ed928b2decba5096
Iskanderrus <a.n.chasovskoy@gmail.com>
2023-04-03 20:58:18 +0300

    final version of the crawlers

--- Project/imdb/imdb/spiders/imdb_spider.py
+++ Project/imdb/imdb/spiders/imdb_spider.py
@@ -11,7 +11,7 @@ class ImdbSpiderSpider(CrawlSpider):
     user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
 
     def start_requests(self):
-        yield scrapy.Request(url="https://www.imdb.com/search/title/?genres=fantasy&explore=title_type,genres", headers={"User_Agent": self.user_agent})
+        yield scrapy.Request(url="https://www.imdb.com/feature/genre/?ref_=nv_ch_gr", headers={"User_Agent": self.user_agent})
 
     rules = (
         Rule(
@@ -46,17 +46,6 @@ class ImdbSpiderSpider(CrawlSpider):
             "genre": response.xpath('//p[@class="text-muted "]/span[5]/text()').get(),
             "votes": response.xpath('//p[@class="sort-num_votes-visible"]/span[@name="nv"][1]/text()').get(),
             "description": response.xpath('//p[@class="text-muted"]/text()').get(),
-            # "team": response.xpath('//div[@class="lister-item-content"]/p[3]/a[1]/text() | //div[@class="lister-item-content"]/p[3]/a[2]/text() | //div[@class="lister-item-content"]/p[3]/a[3]/text() | //div[@class="lister-item-content"]/p[3]/a[4]/text() | //div[@class="lister-item-content"]/p[3]/a[5]/text() | //div[@class="lister-item-content"]/p[3]/a[6]/text()').get(),
-            # "metascore": response.xpath('//span[@class="score-meta"]/text()').get(),
-            # "oscar_nominations": response.xpath('//div[@data-testid="awards"]/ul/li/a/text()').get(),
-            # "wins_and_nominations": response.xpath('//div[@data-testid="awards"]/ul/li/div/ul/li/span/text()').get(),
-            # "director": response.xpath('//section[@data-testid="title-cast"]/ul/li[1]//a/text()').get(),
-            # "country_of_origin": response.xpath('//div[@data-testid="title-details-section"]/ul/li[2]//a/text()').get(),
-            # "production_company": response.xpath(
-            #     '//div[@data-testid="title-details-section"]/ul/li[7]//div//a/text()').get(),
-            # "budget_local_currency": response.xpath('(//div[@data-testid="title-boxoffice-section"]//div/ul/li/span/text())[1]').get(),
-            # "gross_us_canada": response.xpath('(//div[@data-testid="title-boxoffice-section"]//div/ul/li/span/text())[2]').get(),
-            # "opening_weekend_us_canada": response.xpath('//p[@class="sort-num_votes-visible"]/span[@name="nv"][2]/@data-value').get(),
             "movie_url": response.urljoin(response.xpath('//h3/a/@href').get()),
         }
 
