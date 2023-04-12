591df2eff2e669dd5baaeb8fcc5cd2f7fb4f1da5
Iskanderrus <a.n.chasovskoy@gmail.com>
2023-04-12 21:51:11 +0300

    final version of financial, crew and technical film data crawler

--- Project/imdb_financial/imdb_financial/spiders/financial_imdb.py
+++ Project/imdb_financial/imdb_financial/spiders/financial_imdb.py
@@ -10,22 +10,23 @@ class FinancialImdbSpider(CrawlSpider):
     user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
 
     def start_requests(self):
-        yield scrapy.Request(url="https://www.imdb.com/feature/genre/?ref_=nv_ch_gr", headers={"User_Agent": self.user_agent})
+        yield scrapy.Request(url="https://www.imdb.com/feature/genre/?ref_=nv_ch_gr",
+                             headers={"User_Agent": self.user_agent})
 
     rules = (
         Rule(
-            LinkExtractor(restrict_xpaths=('//div[@class="image"]/a')),
+            LinkExtractor(restrict_xpaths='//div[@class="image"]/a'),
             follow=True,
             process_request="set_user_agent"
         ),
         Rule(
-            LinkExtractor(restrict_xpaths=('//h3/a')),
+            LinkExtractor(restrict_xpaths='//h3/a'),
             follow=True,
             callback='parse_item',
             process_request="set_user_agent"
         ),
         Rule(
-            LinkExtractor(restrict_xpaths=('(//a[@class="lister-page-next next-page"])[2]')),
+            LinkExtractor(restrict_xpaths='(//a[@class="lister-page-next next-page"])[2]'),
             follow=True,
             process_request="set_user_agent"
         ),
@@ -35,22 +36,37 @@ class FinancialImdbSpider(CrawlSpider):
         request.headers["User-Agent"] = self.user_agent
         return request
 
-
     def parse_item(self, response):
         yield {
             "title": response.xpath('//h1/span/text()').get(),
-            "country_of_origin": response.xpath('//div[@data-testid="title-details-section"]/ul/li[2]//a/text()').get(),
+            "top_cast": response.xpath('//a[@data-testid="title-cast-item__actor"]/text()').getall(),
+            "director": response.xpath('//ul[@class="ipc-metadata-list ipc-metadata-list--dividers-all sc-bfec09a1-8 iiDmgX ipc-metadata-list--base"]/li[1]//a[contains(@href, "name")]/text()').get(),
+            "writer": response.xpath('//ul[@class="ipc-metadata-list ipc-metadata-list--dividers-all sc-bfec09a1-8 iiDmgX ipc-metadata-list--base"]/li[2]//a[contains(@href, "name")]/text()').get(),
+            "country_of_origin": response.xpath('//a[contains(@href, "country_of_origin")]/text()').getall(),
+            "language": response.xpath('//a[contains(@href, "language")]/text()').get(),
+            "release_date": response.xpath('//div[@class="ipc-metadata-list-item__content-container"]//a[contains(@href, "release")]/text()').get(),
             "production_company": response.xpath(
-                '//div[@data-testid="title-details-section"]/ul/li[6]//ul/li/a/text()').getall(),
-            "budget_local_currency": response.xpath('(//div[@data-testid="title-boxoffice-section"]//div/ul/li/span/text())[1]').get(),
-            "gross_us_canada": response.xpath('(//div[@data-testid="title-boxoffice-section"]//div/ul/li/span/text())[2]').get(),
-            "opening_weekend_us_canada": response.xpath('(//div[@data-testid="title-boxoffice-section"]//div/ul/li/span/text())[3]').get(),
-            "opening_weekend_date": response.xpath('(//div[@data-testid="title-boxoffice-section"]//div/ul/li/span/text())[4]').get(),
+                '//li[@class="ipc-inline-list__item"]/a[contains(@href, "company")]/text()').getall(),
+            "filming_locations": response.xpath(
+                '//div[@class="ipc-metadata-list-item__content-container"]//a[contains(@href, "locations")]/text()').get(),
+            "aspect_ratio": response.xpath(
+                '//li[@data-testid="title-techspec_aspectratio"]/div//span/text()').get(),
+            "budget_local_currency": response.xpath(
+                '//li[@data-testid="title-boxoffice-budget"]//li/span/text()').get(),
+            "gross_us_canada": response.xpath(
+                '//li[@data-testid="title-boxoffice-grossdomestic"]//li/span/text()').get(),
+            "opening_weekend_us_canada_usd": response.xpath(
+                '//li[@data-testid="title-boxoffice-openingweekenddomestic"]//li[1]/span/text()').get(),
+            "opening_weekend_date": response.xpath(
+                '//li[@data-testid="title-boxoffice-openingweekenddomestic"]//li[2]/span/text()').get(),
+            "gross_worldwide": response.xpath(
+                '//li[@data-testid="title-boxoffice-cumulativeworldwidegross"]//li/span/text()').get(),
             "movie_url": response.urljoin(response.xpath('//h3/a/@href').get()),
         }
 
+
 process = CrawlerProcess(settings={
-    'FEED_URI': './data/films_financials.csv', 
+    'FEED_URI': './data/films_financials.csv',
     'FEED_FORMAT': 'csv'
 })
 
