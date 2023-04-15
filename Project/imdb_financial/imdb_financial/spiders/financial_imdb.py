18a3c2eb5a43763a19fcf3520d8a0540b73b8a5b
Iskanderrus <a.n.chasovskoy@gmail.com>
2023-04-15 09:44:35 +0300

    superhero.py must be adjusted by adding the Selenium part

--- Project/imdb_financial/imdb_financial/spiders/financial_imdb.py
+++ Project/imdb_financial/imdb_financial/spiders/financial_imdb.py
@@ -9,6 +9,7 @@ class FinancialImdbSpider(CrawlSpider):
     allowed_domains = ['www.imdb.com']
     user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
 
+    # TODO: Use superhero link as initial. Add Selenium part to unclick the superhero category. Use less layers for scraping.
     def start_requests(self):
         yield scrapy.Request(url="https://www.imdb.com/feature/genre/?ref_=nv_ch_gr",
                              headers={"User_Agent": self.user_agent})
