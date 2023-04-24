72242c949de4aafeca55e5c169ed12a53adc27af
Iskanderrus <a.n.chasovskoy@gmail.com>
2023-04-24 15:26:47 +0300

    added wait time to the financials crawler

--- Project/imdb_financial/imdb_financial/spiders/financials.py
+++ Project/imdb_financial/imdb_financial/spiders/financials.py
@@ -1,3 +1,5 @@
+import time
+
 import pandas as pd
 import scrapy
 from scrapy.crawler import CrawlerProcess
@@ -14,6 +16,7 @@ class FinancialsSpider(scrapy.Spider):
 
     def start_requests(self):
         for d in self.start_urls:
+            time.sleep(2)
             yield Request(d,
                           callback=self.parse,
                           headers={"User-Agent": self.user_agent}
